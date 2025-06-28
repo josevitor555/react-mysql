ac3219089066fe0a2e8f937ecebdb731850c346c
josevitor555 <koservitor123@gmail.com>
2025-06-27 21:12:16 -0300

    Front-End in development

---
+++ react-auth/src/App.tsx
@@ -0,0 +1,17 @@
+
+// Config style css
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="app">
+        <h1 className='text-3xl font-bold underline'> React Auth With MySQL </h1>
+      </div>
+    </>
+  );
+}
+
+export default App
