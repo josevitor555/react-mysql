b6bb994163405eb6e1d6cf1ac7bed8b4035ba4bc
josevitor555 <koservitor123@gmail.com>
2025-06-30 00:29:56 -0300

    Front-End quase... feito

--- react-auth/src/components/LoginScreen.tsx
+++ react-auth/src/components/LoginScreen.tsx
@@ -49,14 +49,14 @@ const LoginScreen = ({ onSwitchToRegister }: LoginScreenProps) => {
             </div>
 
             {/* Right Column - Image */}
-            <div className="hidden lg:flex flex-1 h-screen relative">
+            <div className="hidden lg:flex w-auto max-w-[600px] h-screen relative">
                 <div className="absolute inset-0 bg-gradient-to-br from-[#0A0A0B]/20 to-[#2A2A2C]/20 z-10" />
                 <img
                     src={me}
                     alt="Me, hahaha"
-                    className="w-full h-full object-cover"
+                    className="w-full h-full object-contain"
                 />
-                <div className="absolute bottom-8 left-8 z-20 text-white">
+                <div className="absolute bottom-8 left-30 z-20 text-white text-center">
                     <h3 className="text-2xl font-bold mb-4"> About Me </h3>
                     <p className="text-white/80 max-w-sm">
                         I'm José Vitor, a dev who programs with his soul, questions with his heart, and always finds a way to make even the scroll bar more beautiful.
